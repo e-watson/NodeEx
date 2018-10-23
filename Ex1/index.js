b5469cccbcd3d7ed4634e63e17965158d4989341
e-watson <eduard.skripchenko@icloud.com>
2018-10-23 07:45:34 +0300

    Ex1 init

---
+++ Ex1/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const url = require('url');
+
+const portNumber = 3000;
+const server = http.createServer((req, res) => {
+  const parsedUrlStructure = url.parse(req.url, true);
+  const path = parsedUrlStructure.pathname;
+
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const httpMethod = req.method.toLowerCase();
+  const queryStruct = parsedUrlStructure.query;
+  const headers = req.headers;
+
+  res.end('Hello, world!\n');
+  console.log(`Request received on path: ${trimmedPath} with method: ${httpMethod}\n`);
+  console.log(headers);
+});
+
+server.listen(portNumber, () => {
+  console.log(`Server has been started on port ${portNumber}`);
+});
